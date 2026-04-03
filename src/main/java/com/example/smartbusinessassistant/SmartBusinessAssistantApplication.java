8f609336ea57dc5eea3466ef54d239936fa5b16e
nlafdali <lafdali.nadia7@gmail.com>
2026-04-03 22:08:04 +0200

    initial commit: spring boot projekt setup

---
+++ src/main/java/com/example/smartbusinessassistant/SmartBusinessAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartbusinessassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBusinessAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBusinessAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartbusinessassistant/SmartBusinessAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartbusinessassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBusinessAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
