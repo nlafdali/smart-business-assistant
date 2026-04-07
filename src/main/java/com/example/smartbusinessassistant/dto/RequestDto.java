ebc13b0c0e560d21c803c487f523ce292be2dbc7
nlafdali <lafdali.nadia7@gmail.com>
2026-04-07 22:32:49 +0200

    Datenbank-Structur

---
+++ src/main/java/com/example/smartbusinessassistant/dto/RequestDto.java
@@ -0,0 +1,60 @@
+package com.example.smartbusinessassistant.dto;
+
+public class RequestDto {
+
+    private Long id;
+    private String text;
+    private String category;
+    private String priority;
+    private String status;
+
+    public RequestDto() {}
+
+    public RequestDto(Long id, String text, String category, String priority, String status) {
+        this.id = id;
+        this.text = text;
+        this.category = category;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
